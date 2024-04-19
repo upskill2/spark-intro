9898275106371b25d8a2c032d4e02f2289fcd08e
taras.chmeruk <taras.chmeruk@gameloft.com>
2024-04-19 12:38:53 +0300

    task

--- part1/src/main/java/com/spark/learning/util/ViewingFigures.java
+++ part1/src/main/java/com/spark/learning/util/ViewingFigures.java
@@ -9,7 +9,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import scala.Int;
 import scala.Tuple2;
 
 /**
@@ -26,7 +25,7 @@ public class ViewingFigures {
         JavaSparkContext sc = new JavaSparkContext (conf);
 
         // Use true to use hardcoded data identical to that in the PDF guide.
-        boolean testMode = true;
+        boolean testMode = false;
 
         JavaPairRDD<Integer, Integer> viewData = setUpViewDataRdd (sc, testMode);
         JavaPairRDD<Integer, Integer> chapterData = setUpChapterDataRdd (sc, testMode);
@@ -41,11 +40,73 @@ public class ViewingFigures {
         // TODO - over to you!
 
         //Step 1 - remove any duplicate views
-        final JavaPairRDD<Integer, Integer> removingDupes = viewData.distinct ();
+        final JavaPairRDD<Integer, Integer> removingDupes = viewData.distinct ()
+                ;
         final List<Tuple2<Integer, Integer>> checkDupesRemoved = removingDupes.collect ();
 
         //Step 2 - Joining to get Course Id
-        removingDupes.mapToPair (Tuple2::swap).join (chapterData);
+        final JavaPairRDD<Integer, Tuple2<Integer, Integer>> joinChapterAndCourse = removingDupes
+                .mapToPair (Tuple2::swap).join (chapterData.distinct ());
+        final List<Tuple2<Integer, Tuple2<Integer, Integer>>> checkJoin = joinChapterAndCourse.collect ();
+
+        //Step 3 - Drop the Chapter Id
+        final JavaPairRDD<Tuple2<Integer, Integer>, Integer> dropChapter = joinChapterAndCourse
+                .mapToPair (tuple -> new Tuple2<> (tuple._2, 1));
+        final List<Tuple2<Tuple2<Integer, Integer>, Integer>> checkDrop = dropChapter.collect ();
+
+
+        //Step 4 - Count views
+        final JavaPairRDD<Tuple2<Integer, Integer>, Integer> userAndViews = dropChapter.reduceByKey (Integer::sum);
+        final List<Tuple2<Tuple2<Integer, Integer>, Integer>> checkUsersAndViews = userAndViews.collect ();
+
+        //Step 5 - Drop the user
+        final JavaPairRDD<Integer, Integer> dropUser = userAndViews
+                .mapToPair (tuple -> new Tuple2<> (tuple._1._2, tuple._2));
+        final List<Tuple2<Integer, Integer>> checkDropUser = dropUser.collect ();
+
+
+        //Step 6 - Of how many chapters
+        final JavaPairRDD<Integer, Integer> numberOfCourseinChapter = chapterData
+                .distinct ()
+                .mapToPair (tuple -> new Tuple2<> (tuple._2, 1))
+                .reduceByKey (Integer::sum);
+        final List<Tuple2<Integer, Integer>> checkTotalNumberOfCourses = numberOfCourseinChapter.collect ();
+
+        final JavaPairRDD<Integer, Tuple2<Integer, Integer>> ofHowManyChapters = dropUser.
+                leftOuterJoin (numberOfCourseinChapter)
+                .mapToPair (tuple -> new Tuple2<> (tuple._1, new Tuple2<> (tuple._2._1, tuple._2._2 ().orElse (1))));
+        final List<Tuple2<Integer, Tuple2<Integer, Integer>>> checkOfHowMany = ofHowManyChapters.collect ();
+
+        //Step 7  - convert to percentage
+        final JavaPairRDD<Integer, Double> percenatge = ofHowManyChapters
+                .mapToPair (tuple -> new Tuple2<Integer, Double> (tuple._1,
+                        Double.valueOf (tuple._2._1) / Double.valueOf (tuple._2._2)
+                        //(Optional.ofNullable (tuple._2._2).orElse (1)
+                ));
+        final List<Tuple2<Integer, Double>> checkPercentage = percenatge.collect ();
+
+        //Step 8 - Convert to Scores
+        final JavaPairRDD<Integer, Integer> mapToScores = percenatge.mapValues (v -> {
+            if (v >= 0.9) {
+                return 10;
+            } else if (v >= 0.5 && v < 0.9) {
+                return 4;
+            } else if (v >= 0.25 && v < 0.5) {
+                return 2;
+            }
+            return 0;
+        });
+        final List<Tuple2<Integer, Integer>> listMapToScore = mapToScores.collect ();
+
+        //Step 9 - Add up scores
+        final JavaPairRDD<Integer, Integer> addUpScores = mapToScores.reduceByKey ((x, y) -> x + y);
+        final List<Tuple2<Integer, Integer>> listAddUp = addUpScores.collect ();
+
+        //Exercise 3
+        final JavaPairRDD<String, Integer> addNames = titlesData.leftOuterJoin (addUpScores)
+                .mapToPair (tuple -> new Tuple2<> (tuple._2._1, tuple._2._2.orElse (-1)));
+
+        final List<Tuple2<String, Integer>> listAddnames = addNames.collect ();
 
         sc.close ();
     }
@@ -60,7 +121,7 @@ public class ViewingFigures {
             rawTitles.add (new Tuple2<> (3, "Content Creation is a Mug's Game"));
             return sc.parallelizePairs (rawTitles);
         }
-        return sc.textFile ("src/main/resources/viewing figures/titles.csv")
+        return sc.textFile ("C:\\Users\\taras.chmeruk\\IdeaProjects\\spark-intro\\part1\\src\\main\\resources\\viewing figures\\titles.csv")
                 .mapToPair (commaSeparatedLine -> {
                     String[] cols = commaSeparatedLine.split (",");
                     return new Tuple2<Integer, String> (Integer.valueOf (cols[0]), cols[1]);
@@ -90,7 +151,7 @@ public class ViewingFigures {
             return sc.parallelizePairs (rawChapterData);
         }
 
-        return sc.textFile ("src/main/resources/viewing figures/chapters.csv")
+        return sc.textFile ("C:\\Users\\taras.chmeruk\\IdeaProjects\\spark-intro\\part1\\src\\main\\resources\\viewing figures\\chapters.csv")
                 .mapToPair (commaSeparatedLine -> {
                     String[] cols = commaSeparatedLine.split (",");
                     return new Tuple2<Integer, Integer> (Integer.valueOf (cols[0]), Integer.valueOf (cols[1]));
@@ -112,7 +173,7 @@ public class ViewingFigures {
             return sc.parallelizePairs (rawViewData);
         }
 
-        return sc.textFile ("src/main/resources/viewing figures/views-*.csv")
+        return sc.textFile ("C:\\Users\\taras.chmeruk\\IdeaProjects\\spark-intro\\part1\\src\\main\\resources\\viewing figures\\views-*.csv")
                 .mapToPair (commaSeparatedLine -> {
                     String[] columns = commaSeparatedLine.split (",");
                     return new Tuple2<Integer, Integer> (Integer.valueOf (columns[0]), Integer.valueOf ((columns[1])));
